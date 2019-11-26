597f4e2dbafedceb051f29cae40348d1e77b5421
VibhorBhatnagar <VibhorBhatnagar@9.85.112.125>
2019-11-26 17:29:30 +0530

    Spring Boot

---
+++ TestSpringBootHello/src/test/java/com/ibm/test/TestSpringBootHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
