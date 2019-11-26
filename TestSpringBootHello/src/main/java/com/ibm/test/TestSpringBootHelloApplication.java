8cee2d444f8409d9411bee09f23a3c36f1c82292
VibhorBhatnagar <VibhorBhatnagar@9.85.112.125>
2019-11-26 17:29:30 +0530

    Spring Boot

---
+++ TestSpringBootHello/src/main/java/com/ibm/test/TestSpringBootHelloApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootHelloApplication.class, args);
+	}
+
+}
